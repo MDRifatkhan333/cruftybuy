158dfc4aaee3b0a03476e6ca3f7ea970f36d0b37
RIF@T <67112433+MDRifatkhan333@users.noreply.github.com>
2023-09-10 20:54:00 +0600

    splash screen has been added

--- lib/presentation/ui/screen/splashScreen.dart
+++ lib/presentation/ui/screen/splashScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,18 +11,32 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).colorScheme.primary,
-      child: const Center(
-        child: Text(
-          'CruftyBuy',
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        
+        
+        children: [
+          const Spacer(),
+          Center(
+            child: SvgPicture.asset(
+              'assets/images/logo.svg',
+            ),
           ),
-        ),
+          const Spacer(),
+          const CircularProgressIndicator(),
+          const SizedBox(height: 20),
+          const Text(
+            'Version 1.0.0',
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 20),
+        ],
       ),
     );
+    
   }
 }
